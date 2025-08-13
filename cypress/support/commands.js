198e2414b8513c854c8059bac5f07c9649790872
Ricardo <ricksilvatore@gmail.com>
2025-08-13 01:34:09 -0300

    Ajuste no commands, adicionando método safetype

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,24 +1,24 @@
 Cypress.Commands.add("novoCadastroELogin", ({ nome, email, senha, confirmarSenha }) => {
   cy.contains('button', 'Registrar').click()
-  cy.get('input[type="name"]').clear({ force: true }).type(nome, { force: true })
-  cy.get('.card__register input[name="email"]').clear({ force: true }).type(email, { force: true })
-  cy.get('.card__register input[name="password"]').clear({ force: true }).type(senha, { force: true })
-  cy.get('input[name="passwordConfirmation"]').clear({ force: true }).type(confirmarSenha, { force: true })
+  cy.get('input[name="email"]').eq(1).safeType(email)
+  cy.get('input[type="name"]').safeType(nome)
+  cy.get('.card__register input[name="password"]').safeType(senha)
+  cy.get('input[name="passwordConfirmation"]').safeType(confirmarSenha)
   cy.get('#toggleAddBalance').click({ force: true })
   cy.get('button').contains('Cadastrar').click({ force: true })
-  cy.get('#btnCloseModal').click({ force: true })
+  cy.get('#btnCloseModal').click()
 
-  cy.get('input[name="email"]').eq(0).clear({ force: true }).type(email, { force: true })
-  cy.get('input[name="password"]').eq(0).clear({ force: true }).type(senha, { force: true })
+  cy.get('input[name="email"]').eq(0).safeType(email)
+  cy.get('input[name="password"]').eq(0).safeType(senha)
   cy.get('button').contains('Acessar').click()
 })
 
 Cypress.Commands.add("cadastrarUsuario", ({ nome, email, senha, confirmarSenha, saldo = false }) => {
   cy.contains('button', 'Registrar').click()
-  cy.get('input[type="name"]').clear({ force: true }).type(nome, { force: true })
-  cy.get('.card__register input[name="email"]').clear({ force: true }).type(email, { force: true })
-  cy.get('.card__register input[name="password"]').clear({ force: true }).type(senha, { force: true })
-  cy.get('input[name="passwordConfirmation"]').clear({ force: true }).type(confirmarSenha, { force: true })
+  cy.get('input[type="name"]').safeType(nome)
+  cy.get('.card__register input[name="email"]').safeType(email)
+  cy.get('.card__register input[name="password"]').safeType(senha)
+  cy.get('input[name="passwordConfirmation"]').safeType(confirmarSenha)
 
   cy.get('#toggleAddBalance').then($toggle => {
     const isChecked = $toggle.prop('checked')
@@ -42,10 +42,10 @@ Cypress.Commands.add("cadastrarUsuario", ({ nome, email, senha, confirmarSenha, 
 
 Cypress.Commands.add("cadastrarUsuarioSemSaldo", ({ nome, email, senha, confirmarSenha }) => {
   cy.contains('button', 'Registrar').click()
-  cy.get('input[type="name"]').clear({ force: true }).type(nome, { force: true })
-  cy.get('.card__register input[name="email"]').clear({ force: true }).type(email, { force: true })
-  cy.get('.card__register input[name="password"]').clear({ force: true }).type(senha, { force: true })
-  cy.get('input[name="passwordConfirmation"]').clear({ force: true }).type(confirmarSenha, { force: true })
+  cy.get('input[type="name"]').safeType(nome)
+  cy.get('.card__register input[name="email"]').safeType(email)
+  cy.get('.card__register input[name="password"]').safeType(senha)
+  cy.get('input[name="passwordConfirmation"]').safeType(confirmarSenha)
 
   cy.get('#toggleAddBalance').then($toggle => {
     const isChecked = $toggle.prop('checked')
@@ -57,4 +57,8 @@ Cypress.Commands.add("cadastrarUsuarioSemSaldo", ({ nome, email, senha, confirma
 
   cy.get('button').contains('Cadastrar').click({ force: true })
   cy.get('#btnCloseModal').click({ force: true })
+})
+
+Cypress.Commands.add('safeType', { prevSubject: 'element' }, (subject, text) => {
+  cy.wrap(subject).clear({ force: true }).type(text, { force: true })
 })
